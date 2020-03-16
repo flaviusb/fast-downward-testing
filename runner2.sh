6bc82b69d5a6a83e560a943e2f39d42a0e457af6
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2020-03-17 01:52:16 +1300

    Add options into runner2.sh

--- runner2.sh
+++ runner2.sh
@@ -2,5 +2,56 @@
 
 downward=`ls ~/code/Fast-Downward-c9844230bcf2/builds/release/bin/downward`
 sas=`ls ~/code/Fast-Downward-c9844230bcf2/output.sas`
+
+usage() { cat <<HELP
+runner2.sh: Memory size measuring harness for fast downward.
+Options:
+  --downward  PATH
+  --sas       PATH
+
+Any argument not given will use a default value.
+
+HELP
+exit 0;
+}
+
+while [[ $# > 0 ]]; do
+    if [ $# = 1 ]; then
+      opt="$1"
+      case "${opt}" in
+        -h)
+          usage
+          ;;
+        --help)
+          usage
+          ;;
+        *)
+          echo "I don't understand: $opt"
+          exit 1
+          ;;
+      esac
+    fi
+    opt="$1"
+    value="$2"
+    case "${opt}" in
+      --downward)
+        downward="$value"
+        shift
+        shift
+        ;;
+      --sas)
+        sas="$value"
+        shift
+        shift
+        ;;
+      *)
+        echo "I don't understand: $opt"
+        exit 1
+        ;;
+    esac
+done
+
+
+
 cat "$sas" | gdb -se="$downward" -x gdb.x
 #gdb -se="$downward" -x gdb.x
